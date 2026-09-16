4a68fa0b5ca37e77260a36184ff22a075b98476b
Vicente <vicenteelysobral@gmail.com>
2026-09-16 19:29:19 -0300

    estrutura

--- src/model/Colaborador.java
+++ src/model/Colaborador.java
@@ -9,31 +9,28 @@ public abstract class Colaborador {
     private double salarioBase;
 
 
-
     public Colaborador(String matriculaColaborador, String nomeDoColaborador, double salarioBaseDoColaborador) {
 
         setNome(nomeDoColaborador);
         setSalarioBase(salarioBaseDoColaborador);
-
-        this.matricula = matriculaColaborador;
-        this.nome = nomeDoColaborador;
-        this.salarioBase = salarioBaseDoColaborador;
+        if (matriculaColaborador == null || matriculaColaborador.trim().isEmpty()) { //Validar ANTES de Atribuir (Regra de Ouro da Programação Defensiva)
+            throw new IllegalArgumentException("Erro! A matricula do colaborador é obrigatória.");
+        } else
+            this.matricula = matriculaColaborador;
 
     }
 
     public abstract String getTipoColaborador();
+
     public abstract double calculaAdicionais();
 
-    public double calculaSalarioTotal(){
+    public double calculaSalarioTotal() {
         double salarioTotal = (salarioBase + calculaAdicionais());
         return salarioTotal;
     }
 
 
     public String getMatricula() {
-       if (matricula.trim().isEmpty()){
-           throw new IllegalArgumentException("Erro! A matricula do colaborador é obrigatória.");
-       }else
         return matricula;
     }
 
@@ -42,10 +39,9 @@ public abstract class Colaborador {
     }
 
     public void setNome(String nome) {
-        this.nome = nome;
-        if (nome == null || nome.trim().isEmpty()){ // Precisa do perador logico (|| = OU) pois apenas, 'nome.trim().isEmpty()' assum que o nome não será null
+        if (nome == null || nome.trim().isEmpty()) { // Precisa do perador logico (|| = OU) pois apenas, 'nome.trim().isEmpty()' assum que o nome não será null
             throw new IllegalArgumentException("Erro! O nome do colaborador é obrigatório.");
-        }
+        } else this.nome = nome;
     }
 
     public double getSalarioBase() {
@@ -53,12 +49,11 @@ public abstract class Colaborador {
     }
 
     public void setSalarioBase(double salarioBase) {
-        this.salarioBase = salarioBase;
         if (salarioBase < 0) {
             throw new IllegalArgumentException("Erro! O Valor do salario está incorreto.");
-        }
+        } else this.salarioBase = salarioBase;
     }
-
-
 }
+
+
 

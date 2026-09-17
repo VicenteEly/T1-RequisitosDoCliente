7edf29a17dc3de7476840cb1c93e55658b6f7a9f
Vicente <vicenteelysobral@gmail.com>
2026-09-17 18:42:12 -0300

    Correções da Classe Colaborador e criação da subclasse ColaboradorPadrao

---
+++ src/model/ColaboradorPadrao.java
@@ -0,0 +1,22 @@
+package model;
+
+public class ColaboradorPadrao extends Colaborador{
+
+
+    public ColaboradorPadrao (String matriculaColaboradorPadrao, String nomeColaboradorPadrao, double salarioColaboradorPadrao){
+       super();
+
+    }
+
+
+    @Override // Esse Override ele informa o compilador que está implementando algum metodo da classe pai
+    // Ele também garante que esta utilizando o PROPRIO METODO da classe pai
+    public String getTipoColaborador() {
+        return "";
+    }
+
+    @Override
+    public double calculaAdicionais() {
+        return 0;
+    }
+}

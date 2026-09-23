c61473def2c5b75c5d1f0c574262b1ba1aed4f09
Vicente <vicenteelysobral@gmail.com>
2026-09-23 18:26:52 -0300

    Entity Producao

--- src/main/java/colaboradores/entity/Producao.java
+++ src/main/java/colaboradores/entity/Producao.java
@@ -1,4 +1,27 @@
 package colaboradores.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Table(name = "producao")
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Producao {
+
+    @Column(name = "quantidadeProduzida")
+    private int quantidadeProduzida;
+
+    @Column(name = "valor")
+    private BigDecimal valorUnidade;
+
+    @Column(name = "total")
+    private BigDecimal total;
 }

6f3dbba033bd790713ea5cdfdbcfac6d4884f363
Vicente <vicenteelysobral@gmail.com>
2026-09-23 18:22:43 -0300

    Entity Comissao

--- src/main/java/colaboradores/entity/Comissao.java
+++ src/main/java/colaboradores/entity/Comissao.java
@@ -1,4 +1,31 @@
 package colaboradores.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Table(name = "comissao")
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Comissao {
+
+    @Column(name = "valorVendas")
+    private BigDecimal valorVendas;
+
+    @Column(name = "percentual")
+    private double porcentagem;
+
+    @Column(name = "comissao")
+    private BigDecimal comissao;
+
+    @Column(name = "idMatricula")
+    private String matriculaColaborador;
+
 }

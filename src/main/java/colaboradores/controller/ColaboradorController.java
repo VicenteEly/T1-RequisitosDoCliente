37f4d64926a46d30f970495027a2acd4b3c4fb70
Diogo Fernandes <diogosf2006@gmail.com>
2026-09-23 19:46:25 -0300

    pastas e colaboradorController

---
+++ src/main/java/colaboradores/controller/ColaboradorController.java
@@ -0,0 +1,11 @@
+package colaboradores.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+@RestController
+@RequestMapping("/colaborador")
+public class ColaboradorController {
+}
